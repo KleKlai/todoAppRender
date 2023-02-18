958ecf92312a19bab39a16ba071c78beb7a9b048
Kleklai <mmagallen_180000002192@uic.edu.ph>
2023-02-19 02:45:13 +0800

    Disable cors for testing cors

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kleklai/todoAppv1/graph"
 	"github.com/kleklai/todoAppv1/repository"
@@ -37,12 +36,12 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:  []string{"*"},
-		AllowMethods:  []string{"GET", "POST", "PUT", "OPTIONS"},
-		AllowHeaders:  []string{"Origin", "Content-Type", "Accept-Encoding", "accept", "origin", "Cache-Control", "X-Requested-With"},
-		ExposeHeaders: []string{"Content-Length"},
-	}))
+	// r.Use(cors.New(cors.Config{
+	// 	AllowOrigins:  []string{"*"},
+	// 	AllowMethods:  []string{"GET", "POST", "PUT", "OPTIONS"},
+	// 	AllowHeaders:  []string{"Origin", "Content-Type", "Accept-Encoding", "accept", "origin", "Cache-Control", "X-Requested-With"},
+	// 	ExposeHeaders: []string{"Content-Length"},
+	// }))
 
 	r.POST("/query", graphHandler())
 	r.GET("/playground", playgroundHandler())
